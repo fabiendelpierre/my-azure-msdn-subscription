5b49894f6d07db7e7853d9d73b07d41656a80bff
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2020-12-16 16:05:33 -0500

    add DNS zone

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 resource "azurerm_resource_group" "msdn_sandbox" {
-  name = "${var.base_name}-rg"
-  location = var.azure_region
+  name      = "${var.base_name}-rg"
+  location  = var.azure_region
+
+  tags = local.tags
+}
+
+resource "azurerm_dns_zone" "msdn_sandbox" {
+  name                = var.sandbox_domain_name
+  resource_group_name = azurerm_resource_group.msdn_sandbox.name
 
   tags = local.tags
 }

e6b3f416761aa67624752dfcaf8e55a45e1cc219
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-07-14 14:02:21 -0400

    more cleanup

--- variables.tf
+++ variables.tf
@@ -1,47 +1,47 @@
-variable "base_name" {
-  type = string
-}
+# variable "base_name" {
+#   type = string
+# }
 
-variable "azure_region" {
-  type = string
-}
+# variable "azure_region" {
+#   type = string
+# }
 
-variable "sandbox_domain_name" {
-  type = string
-}
+# variable "sandbox_domain_name" {
+#   type = string
+# }
 
-variable "vnet_cidr" {
-  type = string
-}
+# variable "vnet_cidr" {
+#   type = string
+# }
 
-variable "authorized_cidrs" {
-  type = list(string)
-}
+# variable "authorized_cidrs" {
+#   type = list(string)
+# }
 
-variable "authorized_entities" {
-  type = map(any)
-}
+# variable "authorized_entities" {
+#   type = map(any)
+# }
 
-variable "vault_version" {
-  type = string
-}
+# variable "vault_version" {
+#   type = string
+# }
 
-variable "vault_hostname" {
-  type = string
-}
+# variable "vault_hostname" {
+#   type = string
+# }
 
-variable "vm_admin_username" {
-  type = string
-}
+# variable "vm_admin_username" {
+#   type = string
+# }
 
-variable "vm_admin_public_key" {
-  type = string
-}
+# variable "vm_admin_public_key" {
+#   type = string
+# }
 
-variable "azure_dns_client_id" {
-  type = string
-}
+# variable "azure_dns_client_id" {
+#   type = string
+# }
 
-variable "azure_dns_client_secret" {
-  type = string
-}
+# variable "azure_dns_client_secret" {
+#   type = string
+# }

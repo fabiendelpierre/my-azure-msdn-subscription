fa09b888006b62c37462cb3783e4233cf3233b61
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-07-14 13:46:11 -0400

    temporary cleanup

--- main.tf
+++ main.tf
@@ -1,79 +1,79 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "msdn_sandbox" {
-  name     = "${var.base_name}-rg"
-  location = var.azure_region
+# resource "azurerm_resource_group" "msdn_sandbox" {
+#   name     = "${var.base_name}-rg"
+#   location = var.azure_region
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_dns_zone" "msdn_sandbox" {
-  name                = var.sandbox_domain_name
-  resource_group_name = azurerm_resource_group.msdn_sandbox.name
+# resource "azurerm_dns_zone" "msdn_sandbox" {
+#   name                = var.sandbox_domain_name
+#   resource_group_name = azurerm_resource_group.msdn_sandbox.name
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-# Delegation to my sandbox DNS zone in AWS Route 53
-resource "azurerm_dns_ns_record" "aws_sandbox" {
-  name                = "aws"
-  zone_name           = azurerm_dns_zone.msdn_sandbox.name
-  resource_group_name = azurerm_resource_group.msdn_sandbox.name
-  ttl                 = 3600
+# # Delegation to my sandbox DNS zone in AWS Route 53
+# resource "azurerm_dns_ns_record" "aws_sandbox" {
+#   name                = "aws"
+#   zone_name           = azurerm_dns_zone.msdn_sandbox.name
+#   resource_group_name = azurerm_resource_group.msdn_sandbox.name
+#   ttl                 = 3600
 
-  records = [
-    "ns-1746.awsdns-26.co.uk",
-    "ns-920.awsdns-51.net",
-    "ns-449.awsdns-56.com",
-    "ns-1440.awsdns-52.org.",
-  ]
+#   records = [
+#     "ns-1746.awsdns-26.co.uk",
+#     "ns-920.awsdns-51.net",
+#     "ns-449.awsdns-56.com",
+#     "ns-1440.awsdns-52.org.",
+#   ]
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_virtual_network" "msdn_sandbox" {
-  name                = "${var.base_name}-vnet"
-  location            = var.azure_region
-  resource_group_name = azurerm_resource_group.msdn_sandbox.name
-  address_space       = [var.vnet_cidr]
+# resource "azurerm_virtual_network" "msdn_sandbox" {
+#   name                = "${var.base_name}-vnet"
+#   location            = var.azure_region
+#   resource_group_name = azurerm_resource_group.msdn_sandbox.name
+#   address_space       = [var.vnet_cidr]
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_subnet" "msdn_sandbox1" {
-  name                 = "${var.base_name}-vnet-subnet1"
-  resource_group_name  = azurerm_resource_group.msdn_sandbox.name
-  virtual_network_name = azurerm_virtual_network.msdn_sandbox.name
-  address_prefixes     = [cidrsubnet(var.vnet_cidr, 2, 0)]
+# resource "azurerm_subnet" "msdn_sandbox1" {
+#   name                 = "${var.base_name}-vnet-subnet1"
+#   resource_group_name  = azurerm_resource_group.msdn_sandbox.name
+#   virtual_network_name = azurerm_virtual_network.msdn_sandbox.name
+#   address_prefixes     = [cidrsubnet(var.vnet_cidr, 2, 0)]
 
-  service_endpoints = [
-    "Microsoft.AzureActiveDirectory",
-    "Microsoft.ContainerRegistry",
-    "Microsoft.KeyVault",
-    "Microsoft.Storage",
-  ]
-}
+#   service_endpoints = [
+#     "Microsoft.AzureActiveDirectory",
+#     "Microsoft.ContainerRegistry",
+#     "Microsoft.KeyVault",
+#     "Microsoft.Storage",
+#   ]
+# }
 
-resource "azurerm_network_security_group" "msdn_sandbox1" {
-  name                = "${azurerm_subnet.msdn_sandbox1.name}-nsg"
-  resource_group_name = azurerm_resource_group.msdn_sandbox.name
-  location            = var.azure_region
+# resource "azurerm_network_security_group" "msdn_sandbox1" {
+#   name                = "${azurerm_subnet.msdn_sandbox1.name}-nsg"
+#   resource_group_name = azurerm_resource_group.msdn_sandbox.name
+#   location            = var.azure_region
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_subnet_network_security_group_association" "msdn_sandbox1" {
-  subnet_id                 = azurerm_subnet.msdn_sandbox1.id
-  network_security_group_id = azurerm_network_security_group.msdn_sandbox1.id
-}
+# resource "azurerm_subnet_network_security_group_association" "msdn_sandbox1" {
+#   subnet_id                 = azurerm_subnet.msdn_sandbox1.id
+#   network_security_group_id = azurerm_network_security_group.msdn_sandbox1.id
+# }
 
-resource "azurerm_network_security_group" "test" {
-  name                = "${azurerm_subnet.msdn_sandbox1.name}-nsg-test"
-  resource_group_name = azurerm_resource_group.msdn_sandbox.name
-  location            = var.azure_region
+# resource "azurerm_network_security_group" "test" {
+#   name                = "${azurerm_subnet.msdn_sandbox1.name}-nsg-test"
+#   resource_group_name = azurerm_resource_group.msdn_sandbox.name
+#   location            = var.azure_region
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
 # module "key_vault" {
 #   source  = "app.terraform.io/fabiend/keyvault/azurerm"

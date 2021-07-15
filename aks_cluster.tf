0ad1a3b1a7157bed02435fb8195644abfa778866
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-07-15 13:43:04 -0400

    add managed identity for future AKS cluster

---
+++ aks_cluster.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "aks" {
+  name     = "${var.base_name}-aks"
+  location = var.azure_region
+
+  tags = local.tags
+}
+
+resource "azurerm_user_assigned_identity" "aks" {
+  resource_group_name = azurerm_resource_group.aks.name
+  location            = var.azure_region
+  name                = "${var.base_name}-aks-uai"
+}
+
+resource "azurerm_role_assignment" "aks_subnet_network_contributor" {
+  scope                = azurerm_subnet.msdn_sandbox1.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+}
+
+resource "azurerm_role_assignment" "aks_route_table_network_contributor" {
+  scope                = azurerm_route_table.msdn_sandbox.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+}

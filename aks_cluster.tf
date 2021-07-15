32646590d2fc648a50a776a19d2838006f54a52c
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-07-15 13:43:48 -0400

    tag the UAI

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -9,6 +9,7 @@ resource "azurerm_user_assigned_identity" "aks" {
   resource_group_name = azurerm_resource_group.aks.name
   location            = var.azure_region
   name                = "${var.base_name}-aks-uai"
+  tags                = local.tags
 }
 
 resource "azurerm_role_assignment" "aks_subnet_network_contributor" {
